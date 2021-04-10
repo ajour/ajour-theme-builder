0196a131479937164ffd1f574e3a676b2625bb1a
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-04-10 20:47:14 +0200

    Update Gui.svelte

--- src/components/Gui.svelte
+++ src/components/Gui.svelte
@@ -1,5 +1,6 @@
 <script>
   import { theme } from '../theme'
+  const addons = [{ title: 'bigwigs' }, { title: 'hekili' }, { title: 'DBM' }]
 </script>
 
 <div class="gui" style="background-color: {$theme.palette.base.background}">
@@ -27,6 +28,40 @@
         >
           ▼
         </p>
+      </div>
+    </div>
+    <div class="gui-addon-content">
+      <div class="gui-addon-menu">
+        <div
+          class="gui-addon-content-refresh"
+          style="color: {$theme.palette.bright.primary}{'FF'}"
+        >
+          Refresh
+        </div>
+        <div
+          class="gui-addon-content-update-all"
+          style="color: {$theme.palette.bright.primary}{'FF'}"
+        >
+          Update All
+        </div>
+        <div
+          class="gui-addon-content-addons-loaded"
+          style="color: {$theme.palette.normal.surface}{'FF'}"
+        >
+          14 retail addons loaded
+        </div>
+      </div>
+      <div class="gui-addons">
+        {#each addons as { title }}
+          <div
+            class="gui-addon"
+            style="color: {$theme.palette.bright
+              .surface}{'FF'}; background-color: {$theme.palette.base
+              .foreground}{'FF'}"
+          >
+            {title}
+          </div>
+        {/each}
       </div>
     </div>
   </div>
@@ -62,5 +97,32 @@
     margin-left: 10px;
     padding-left: 4px;
     padding-right: 8px;
+  }
+
+  .gui-addon-content {
+    display: flex;
+    flex-direction: column;
+    height: 60px;
+    margin: 12px;
+  }
+
+  .gui-addon-menu {
+    display: flex;
+  }
+  .gui-addon-menu > * + * {
+    margin-left: 10px;
+  }
+
+  .gui-addons {
+    display: flex;
+    flex-direction: column;
+    margin-top: 12px;
+  }
+  .gui-addons > * + * {
+    margin-top: 1px;
+  }
+
+  .gui-addon {
+    height: 26px;
   }
 </style>
