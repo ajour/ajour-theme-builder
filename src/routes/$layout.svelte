e5e57791fa6f61d3d9627130f9e08ed560763b2c
Cory Forsstrom <cforsstrom18@gmail.com>
2021-04-12 14:06:57 -0700

    add base to favicon

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,9 +1,12 @@
 <script>
   import '../app.scss'
+
+  import { base } from '$app/paths'
 </script>
 
 <svelte:head>
   <title>Ajour Theme Builder</title>
+  <link rel="icon" href="{base}/favicon.ico" />
 </svelte:head>
 
 <slot />
