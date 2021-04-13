428f6fa8addc71f35d2a57405965cc4d91dfd054
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-04-13 21:28:50 +0200

    chore: initial css grid to hold colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,8 @@
   import { page } from '$app/stores'
   import { shareModalShown } from '$lib/store'
 
+  import './index.scss'
+
   const onClickExportTheme = () => {
     exportTheme($theme)
   }
@@ -29,11 +31,6 @@
   <div />
   <!-- test input / reactive css color -->
   <p>Background color: {$theme.palette.base.background}</p>
-  <input
-    type="color"
-    bind:value={$theme.palette.base.background}
-    style="height: 50px;"
-  />
   <div />
   <!-- test export theme file -->
   <button on:click={onClickExportTheme}>EXPORT</button>
@@ -42,4 +39,39 @@
   {#if $shareModalShown}
     <Share />
   {/if}
+  <div class="grid-container">
+    <div class="base-theme-colors">Base</div>
+    <div class="normal-theme-colors">Normal</div>
+    <div class="bright-theme-colors">Bright</div>
+    <div class="base-foreground">
+      <input type="color" bind:value={$theme.palette.base.foreground} />
+    </div>
+    <div class="base-background">
+      <input type="color" bind:value={$theme.palette.base.background} />
+    </div>
+    <div class="normal-primary">
+      <input type="color" bind:value={$theme.palette.normal.primary} />
+    </div>
+    <div class="normal-secondary">
+      <input type="color" bind:value={$theme.palette.normal.secondary} />
+    </div>
+    <div class="normal-surface">
+      <input type="color" bind:value={$theme.palette.normal.surface} />
+    </div>
+    <div class="normal-error">
+      <input type="color" bind:value={$theme.palette.normal.error} />
+    </div>
+    <div class="bright-primary">
+      <input type="color" bind:value={$theme.palette.bright.primary} />
+    </div>
+    <div class="bright-secondary">
+      <input type="color" bind:value={$theme.palette.bright.secondary} />
+    </div>
+    <div class="bright-surface">
+      <input type="color" bind:value={$theme.palette.bright.surface} />
+    </div>
+    <div class="bright-error">
+      <input type="color" bind:value={$theme.palette.bright.error} />
+    </div>
+  </div>
 </main>
