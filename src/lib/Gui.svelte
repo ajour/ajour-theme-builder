6ab6807ae209dca5d927cb043e211c469bff00e7
Cory Forsstrom <cforsstrom18@gmail.com>
2021-04-12 17:27:18 -0700

    add expanded row

--- src/lib/Gui.svelte
+++ src/lib/Gui.svelte
@@ -10,6 +10,7 @@
       source: 'CurseForge',
       latest: '1 week ago',
       status: '',
+      isExpanded: false,
     },
     {
       title: 'Venture Plan',
@@ -19,6 +20,17 @@
       source: 'TownlongYak',
       latest: '1 day ago',
       status: 'Update',
+      isExpanded: false,
+    },
+    {
+      title: 'WeakAuras',
+      local: '3.2.3',
+      remote: '3.2.3',
+      gameVersion: '9.0.5',
+      source: 'CurseForge',
+      latest: '1 day ago',
+      status: '',
+      isExpanded: true,
     },
   ]
 </script>
@@ -38,8 +50,8 @@
         class="gui-menu-flavor"
         style="border-color: {$theme.palette.bright.primary}{32}"
       >
-        <p style="color: {$theme.palette.bright.primary}{'FF'}">Retail</p>
-        <p style="color: {$theme.palette.bright.primary}{'FF'}">▼</p>
+        <p style="color: {$theme.palette.bright.primary}">Retail</p>
+        <p style="color: {$theme.palette.bright.primary}">▼</p>
       </div>
       <div
         class="gui-menu-segmented "
@@ -48,14 +60,13 @@
         <div
           class="gui-menu-segmented-button gui-menu-segmented-button-active"
           style="color: {$theme.palette.bright
-            .primary}{'FF'}; background-color: {$theme.palette.bright
-            .primary}{32}"
+            .primary}; background-color: {$theme.palette.bright.primary}{32}"
         >
           <p>My Addons</p>
         </div>
         <div
           class="gui-menu-segmented-button"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           <p>Wago</p>
         </div>
@@ -66,13 +77,13 @@
       >
         <div
           class="gui-menu-segmented-button"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           <p>Catalog</p>
         </div>
         <div
           class="gui-menu-segmented-button"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           <p>Install from URL</p>
         </div>
@@ -87,13 +98,13 @@
       >
         <div
           class="gui-menu-segmented-button"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           <p>About</p>
         </div>
         <div
           class="gui-menu-segmented-button"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           <p>Settings</p>
         </div>
@@ -103,29 +114,29 @@
       <div class="gui-addon-menu">
         <div
           class="gui-addon-content-refresh"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           Refresh
         </div>
         <div
           class="gui-addon-content-update-all"
-          style="color: {$theme.palette.bright.primary}{'FF'}"
+          style="color: {$theme.palette.bright.primary}"
         >
           Update All
         </div>
         <div
           class="gui-addon-content-addons-loaded"
-          style="color: {$theme.palette.normal.surface}{'FF'}"
+          style="color: {$theme.palette.normal.surface}"
         >
           14 retail addons loaded
         </div>
         <div
           class="gui-addon-content-search-container"
-          style="background-color: {$theme.palette.base.foreground}{'FF'}"
+          style="background-color: {$theme.palette.base.foreground}"
         >
           <div
             class="gui-addon-content-search-text"
-            style="color: {$theme.palette.normal.surface}{'FF'}"
+            style="color: {$theme.palette.normal.surface}"
           >
             Search for an addon...
           </div>
@@ -147,50 +158,105 @@
           </th>
           <th style="color: {$theme.palette.bright.primary}"> Status ▼ </th>
         </tr>
-        {#each addons as { title, local, remote, gameVersion, source, latest, status }}
+        {#each addons as { title, local, remote, gameVersion, source, latest, status, isExpanded }}
           <tr>
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.bright
-                .surface}{'FF'}; "
+                .foreground}; color: {isExpanded
+                ? $theme.palette.bright.primary
+                : $theme.palette.bright.surface}; "
             >
               {title}
             </td>
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.normal
-                .surface}{'FF'}; ">{local}</td
+                .foreground}; color: {$theme.palette.normal.surface}; "
+              >{local}</td
             >
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.normal
-                .surface}{'FF'}; ">{remote}</td
+                .foreground}; color: {$theme.palette.normal.surface}; "
+              >{remote}</td
             >
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.normal
-                .surface}{'FF'}; ">{gameVersion}</td
+                .foreground}; color: {$theme.palette.normal.surface}; "
+              >{gameVersion}</td
             >
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.normal
-                .surface}{'FF'}; ">{source}</td
+                .foreground}; color: {$theme.palette.normal.surface}; "
+              >{source}</td
             >
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.normal
-                .surface}{'FF'}; ">{latest}</td
+                .foreground}; color: {$theme.palette.normal.surface}; "
+              >{latest}</td
             >
             <td
               style="background-color: {$theme.palette.base
-                .foreground}{'FF'}; color: {$theme.palette.bright
-                .secondary}{'FF'}; ">{status}</td
+                .foreground}; color: {$theme.palette.bright.secondary}; "
+              >{status}</td
             >
           </tr>
           <div
             class="table-row-gap"
-            style="background-color: {$theme.palette.base.background}{'FF'};"
+            style="background-color: {$theme.palette.base.background};"
           />
+          {#if isExpanded}
+            <tr
+              style="background-color: {$theme.palette.base.foreground}{'CC'}"
+            >
+              <td colspan="7">
+                <div class="expanded-row">
+                  <div>
+                    <p style="color: {$theme.palette.bright.surface}">
+                      Author(s)
+                    </p>
+                    <p style="color: {$theme.palette.normal.surface}">
+                      The WeakAuras Team
+                    </p>
+                  </div>
+                  <div>
+                    <p style="color: {$theme.palette.bright.surface}">
+                      Summary
+                    </p>
+                    <p style="color: {$theme.palette.normal.surface}">
+                      A powerful, comprehensive utility for displaying graphics
+                      and information based on buffs, debuffs, and other
+                      triggers
+                    </p>
+                  </div>
+                  <div>
+                    <p style="color: {$theme.palette.bright.surface}">
+                      Release channel
+                    </p>
+                  </div>
+                  <div class="expanded-row-grow" />
+                  <div class="expanded-row-buttons">
+                    <div>
+                      <p style="color: {$theme.palette.bright.primary}">
+                        Website
+                      </p>
+                    </div>
+                    <div>
+                      <p style="color: {$theme.palette.bright.primary}">
+                        Changelog
+                      </p>
+                    </div>
+                    <div>
+                      <p style="color: {$theme.palette.bright.primary}">
+                        Exclude
+                      </p>
+                    </div>
+                    <div>
+                      <p style="color: {$theme.palette.bright.error}">Delete</p>
+                    </div>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          {/if}
         {/each}
       </table>
     </div>
@@ -320,5 +386,35 @@
 
   .table-row-gap {
     height: 1px;
+  }
+
+  .expanded-row {
+    height: 266px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 20px;
+    padding-top: 20px;
+  }
+
+  .expanded-row > * + * {
+    margin-top: 10px;
+  }
+
+  .expanded-row > * > p {
+    margin: 0px;
+  }
+
+  .expanded-row-grow {
+    flex-grow: 1;
+  }
+
+  .expanded-row-buttons {
+    display: flex;
+    justify-content: right;
+    margin-right: 20px;
+  }
+
+  .expanded-row-buttons > * + * {
+    margin-left: 10px;
   }
 </style>
