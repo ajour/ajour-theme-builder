cb099ae48e054ae93de4414737172f30552490bb
Casper Rogild Storm <casper+github@rogildstorm.com>
2021-04-14 13:01:25 +0200

    chore: optional guard

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,7 @@
   onMount(() => {
     const queryTheme = JSON.parse($page.query.get('theme'))
     console.log('page: ', page)
-    console.log('query: ', page.query.get('theme'))
+    console.log('query: ', page.query?.get('theme'))
     console.log('theme: ', queryTheme)
 
     if (queryTheme) {
